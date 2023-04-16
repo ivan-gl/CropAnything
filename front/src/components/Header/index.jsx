05fdfb101cd816cc42b1a409e7d75b387a58fb32
puftare <martin.potkonjak@hotmail.com>
2023-04-16 14:35:56 +0200

    Change: style

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './header.css';
 
 function Header() {
-  return <div className="header-component-main-div">CropAnything</div>;
+  return (
+    <div className="header-component-main-div">
+      <h2 className="header-component-main-h2">CropAnything</h2>
+    </div>
+  );
 }
 
 export default Header;
